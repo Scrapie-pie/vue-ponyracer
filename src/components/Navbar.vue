71b4f5ed07312f036c3e042735018d4e1643cc9c
Sergey Bochkarev <neontec2@gmail.com>
2023-08-14 11:03:14 +0300

    lesson `#2 Templates` completed

---
+++ src/components/Navbar.vue
@@ -0,0 +1,26 @@
+<template>
+  <nav class="navbar navbar-expand-md navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand">PonyRacer</a>
+      <button @click="toggleNavbar" type="button" class="navbar-toggler">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div id="navbar" class="navbar-collapse" :class="{ collapse: navbarCollapsed }">
+        <ul class="navbar-nav me-auto">
+          <li class="nav-item">
+            <a class="nav-link">Races</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const navbarCollapsed = ref(true);
+const toggleNavbar = () => {
+  navbarCollapsed.value = !navbarCollapsed.value;
+};
+</script>
