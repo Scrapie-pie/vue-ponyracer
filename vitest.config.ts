1120de7b304d36ce8e557768f2e66b0c432b41d5
Sergey Bochkarev <neontec2@gmail.com>
2023-08-14 10:32:13 +0300

    project prepared

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,15 +1,24 @@
-import { fileURLToPath } from 'node:url'
-import { mergeConfig } from 'vite'
-import { configDefaults, defineConfig } from 'vitest/config'
-import viteConfig from './vite.config'
+import { mergeConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
+import viteConfig from './vite.config';
 
 export default mergeConfig(
   viteConfig,
   defineConfig({
     test: {
       environment: 'jsdom',
-      exclude: [...configDefaults.exclude, 'e2e/*'],
-      root: fileURLToPath(new URL('./', import.meta.url))
+      clearMocks: true,
+      reporters: ['default', 'json'],
+      outputFile: 'results/vitest-results.json',
+      sequence: {
+        shuffle: true
+      },
+      coverage: {
+        provider: 'istanbul',
+        all: true,
+        exclude: [...configDefaults.coverage.exclude!, 'src/main.ts', 'src/router.ts', 'src/models'],
+        reporter: ['json-summary', 'text', 'html']
+      }
     }
   })
-)
+);
