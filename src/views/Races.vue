6815aaefb201d478e2bebdbb0e655c4db3571fee
Sergey Bochkarev <neontec2@gmail.com>
2023-08-14 14:16:31 +0300

    lesson `#8 Lifecycle hooks` completed

--- src/views/Races.vue
+++ src/views/Races.vue
@@ -1,36 +1,19 @@
 <template>
-  <Race v-for="race of races" :key="race.id" :raceModel="race" />
+  <div>
+    <Race v-for="race of races" :key="race.id" :raceModel="race" />
+  </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import { RaceModel } from '@/models/RaceModel';
 import Race from '@/components/Race.vue';
+import { useRaceService } from '@/composables/RaceService';
 
-const races = ref<Array<RaceModel>>([
-  {
-    id: 12,
-    name: 'Paris',
-    ponies: [
-      { id: 1, name: 'Gentle Pie', color: 'YELLOW' },
-      { id: 2, name: 'Big Soda', color: 'ORANGE' },
-      { id: 3, name: 'Gentle Bottle', color: 'PURPLE' },
-      { id: 4, name: 'Superb Whiskey', color: 'GREEN' },
-      { id: 5, name: 'Fast Rainbow', color: 'BLUE' }
-    ],
-    startInstant: '2020-02-18T08:02:00Z'
-  },
-  {
-    id: 13,
-    name: 'Tokyo',
-    ponies: [
-      { id: 6, name: 'Fast Rainbow', color: 'BLUE' },
-      { id: 7, name: 'Gentle Castle', color: 'GREEN' },
-      { id: 8, name: 'Awesome Rock', color: 'PURPLE' },
-      { id: 9, name: 'Little Rainbow', color: 'YELLOW' },
-      { id: 10, name: 'Great Soda', color: 'ORANGE' }
-    ],
-    startInstant: '2020-02-18T08:03:00Z'
-  }
-]);
+const raceService = useRaceService();
+const races = ref<Array<RaceModel>>([]);
+
+onMounted(async () => {
+  races.value = await raceService.list();
+});
 </script>
