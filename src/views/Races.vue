0fe024e5eeacf4ba9997cb7a8b208fd9d3e4fec4
Sergey Bochkarev <neontec2@gmail.com>
2023-08-14 11:35:51 +0300

    lesson `#3 List of races` completed

---
+++ src/views/Races.vue
@@ -0,0 +1,13 @@
+<template>
+  <!-- eslint-disable-next-line vue/require-v-for-key -->
+  <div v-for="race of races">
+    <h2>{{ race.name }}</h2>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+import { RaceModel } from '@/models/RaceModel';
+
+const races = ref<Array<RaceModel>>([{ name: 'Lyon' }, { name: 'London' }]);
+</script>
