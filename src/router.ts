817577e1d9a17b9b1f0add8cf3d52198ce36e899
Sergey Bochkarev <neontec2@gmail.com>
2023-08-15 16:42:04 +0300

    lesson `#29 Lazy loading` completed

--- src/router.ts
+++ src/router.ts
@@ -1,12 +1,12 @@
 import { createWebHistory, createRouter, RouteLocationNormalized } from 'vue-router';
 import Home from '@/views/Home.vue';
-import Races from '@/views/Races.vue';
-import PendingRaces from '@/views/PendingRaces.vue';
-import FinishedRaces from '@/views/FinishedRaces.vue';
-import Bet from '@/views/Bet.vue';
-import Live from '@/views/Live.vue';
-import Register from '@/views/Register.vue';
-import Login from '@/views/Login.vue';
+const Races = () => import('@/views/Races.vue');
+const PendingRaces = () => import('@/views/PendingRaces.vue');
+const FinishedRaces = () => import('@/views/FinishedRaces.vue');
+const Bet = () => import('@/views/Bet.vue');
+const Live = () => import('@/views/Live.vue');
+const Register = () => import('@/views/Register.vue');
+const Login = () => import('@/views/Login.vue');
 import { useUserStore } from '@/composables/UserStore';
 
 const router = createRouter({
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite';
+import { configDefaults, defineConfig } from 'vitest/config';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
@@ -9,6 +9,39 @@ export default defineConfig({
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  build: {
+    rollupOptions: {
+      // https://rollupjs.org/guide/en/#outputmanualchunks
+      output: {
+        manualChunks: {
+          races: [
+            './src/views/Races.vue',
+            './src/views/PendingRaces.vue',
+            './src/views/FinishedRaces.vue',
+            './src/views/Bet.vue',
+            './src/views/Live.vue'
+          ],
+          users: ['./src/views/Register.vue', './src/views/Login.vue']
+        }
+      }
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    clearMocks: true,
+    testTimeout: 10000,
+    reporters: ['default', 'json'],
+    outputFile: 'results/vitest-results.json',
+    sequence: {
+      shuffle: true
+    },
+    coverage: {
+      provider: 'istanbul',
+      all: true,
+      exclude: [...configDefaults.coverage.exclude!, 'src/main.ts', 'src/router.ts', 'src/models'],
+      reporter: ['json-summary', 'text', 'html']
     }
   }
 });
